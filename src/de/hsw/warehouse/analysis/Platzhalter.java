83a61f73fb0a9d9bde59738431f68adf46ecf716
Constantin <Constantin@Constantin-PC>
2014-04-24 22:40:14 +0200

    Creating packages testdata and analasys

---
+++ src/de/hsw/warehouse/analysis/Platzhalter.java
@@ -0,0 +1,6 @@
+package de.hsw.warehouse.analysis;
+
+public class Platzhalter
+{
+	//diese Klasse dient nur als Platzhalter und muss wieder gelöscht werden
+}
---
+++ src/de/hsw/warehouse/testdata/Platzhalter.java
@@ -0,0 +1,6 @@
+package de.hsw.warehouse.testdata;
+
+public class Platzhalter
+{
+	//diese Klasse dient nur als Platzhalter und muss wieder gelöscht werden
+}
