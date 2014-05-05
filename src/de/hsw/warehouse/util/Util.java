08d3de6e9350fd05439c5ed5c2db6cb3e3bfad5f
Constantin <Constantin@Constantin-PC>
2014-05-05 08:11:04 +0200

    Changes to parseInputToGregorianCalendar 	added parameter defaultDate 		when input is empty, the defaultDate gets returned 	When input[] contains single Date/Month, method returns period 	representing onr day/month

--- src/de/hsw/warehouse/util/Util.java
+++ src/de/hsw/warehouse/util/Util.java
@@ -10,8 +10,9 @@ import java.util.GregorianCalendar;
 
 public class Util
 {
-	public static GregorianCalendar[] inputDateOrPeriod()
+	public static GregorianCalendar[] inputDateOrPeriod(String message, GregorianCalendar defaultDate)
 	{
+		System.out.print(message);
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String input = "";
 		try {
@@ -20,22 +21,24 @@ public class Util
 			e.printStackTrace();
 		}
 		String[] inputArray = input.split("-");
-		System.out.println(inputArray.length);
-		ArrayList<GregorianCalendar> result = parseInputToGregorianCalendar(inputArray);
+		ArrayList<GregorianCalendar> result = parseInputToGregorianCalendar(inputArray, defaultDate);
 		return result.toArray(new GregorianCalendar[result.size()]);
 	}
 	
-	private static ArrayList<GregorianCalendar> parseInputToGregorianCalendar(String input[])
+	private static ArrayList<GregorianCalendar> parseInputToGregorianCalendar(String input[], GregorianCalendar defaultDate)
 	{
 		ArrayList<GregorianCalendar> result = new ArrayList<GregorianCalendar>();
 		SimpleDateFormat sdf = new SimpleDateFormat();
-		
 		for(int i = 0; i < input.length; i++){
 			sdf.applyPattern("dd.MM.yyy");
-			try {
 			result.add(new GregorianCalendar());
+			try {
 			result.get(i).setTime(sdf.parse(input[i]));
-			if(i != 0){
+			if(input.length == 1){
+				result.add((GregorianCalendar) result.get(i).clone());
+				result.get(i+1).add(GregorianCalendar.DAY_OF_YEAR, 1);
+				result.get(i+1).add(GregorianCalendar.SECOND, -1);
+			} else if(i !=0 ){
 				result.get(i).add(GregorianCalendar.DAY_OF_YEAR, 1);
 				result.get(i).add(GregorianCalendar.SECOND, -1);
 			}
@@ -43,12 +46,16 @@ public class Util
 			sdf.applyPattern("MM.yyy");
 			try {
 				result.get(i).setTime(sdf.parse(input[i]));
-				if(i != 0){
+				if(input.length == 1){
+					result.add((GregorianCalendar) result.get(i).clone());
+					result.get(i+1).add(GregorianCalendar.MONTH, 1);
+					result.get(i+1).add(GregorianCalendar.SECOND, -1);
+				} else if(i !=0 ){
 					result.get(i).add(GregorianCalendar.MONTH, 1);
 					result.get(i).add(GregorianCalendar.SECOND, -1);
 				}
 				} catch (ParseException e1) {
-				e1.printStackTrace();
+					result.set(i, defaultDate);
 				}
 			}
 		}
