ab68f75c4d16b901dc3fa70b2981944c250d1ff4
Constantin <Constantin@Constantin-PC>
2014-05-26 21:39:50 +0200

    Initial documentation of NotEnoughArticleException

--- src/de/hsw/warehouse/model/NotEnoughArticleException.java
+++ src/de/hsw/warehouse/model/NotEnoughArticleException.java
@@ -1,9 +1,17 @@
 package de.hsw.warehouse.model;
 
+/**
+ * Diese Exception wird geworfen, wenn nicht genügend Artikel vorhanden sind.
+ * @author Constantin
+ *
+ */
 public class NotEnoughArticleException extends Exception
 {
 	private static final long serialVersionUID = 2350835064311252083L;
 
+	/**
+	 * Konstruktor für die Exception.
+	 */
 	public NotEnoughArticleException(){
 		super();
 	}
