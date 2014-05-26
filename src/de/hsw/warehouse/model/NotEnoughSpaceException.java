73821a245e385477ff3e84334e17c245742ad496
Constantin <Constantin@Constantin-PC>
2014-05-26 21:44:43 +0200

    Initial documentation of NotEnoughSpaceException.java

--- src/de/hsw/warehouse/model/NotEnoughSpaceException.java
+++ src/de/hsw/warehouse/model/NotEnoughSpaceException.java
@@ -1,9 +1,17 @@
 package de.hsw.warehouse.model;
 
+/**
+ * Diese Exception wird geworfen, wenn nicht genug Platz für einen Artikel vorhanden ist.
+ * @author Constantin
+ * @version
+ */
 public class NotEnoughSpaceException extends Exception
 {
 	private static final long serialVersionUID = -3557633092630798000L;
 
+	/**
+	 * Konstruktor für die Exception.
+	 */
 	public NotEnoughSpaceException(){
 		super();
 	}
