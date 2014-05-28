43c242d76ceeb071da11b8e354e2f41f924bb910
Constantin <Constantin@Constantin-PC>
2014-05-29 01:23:42 +0200

    Initial documentation of Testdata.java

--- src/de/hsw/warehouse/analysis/Testdata.java
+++ src/de/hsw/warehouse/analysis/Testdata.java
@@ -15,12 +15,24 @@ import de.hsw.warehouse.model.NotEnoughSpaceException;
 import de.hsw.warehouse.model.Warehouse;
 import de.hsw.warehouse.util.Util;
 
+/**
+ * Diese Klasse repräsentiert die Testdaten. Ein Testdatensatz entspricht dabei einer {@link de.hsw.warehouse.analysis.Transaction Lagerbewegung}.
+ * Außerdem wird hier das Gesamtvolumen des {@link de.hsw.warehouse.model.Warehouse Lagers} festgehalten, welches den Testdaten zugrunde liegt, sowie Start- und Endzeitpunkt der Testdaten.
+ * @author Nico Tietje, Constantin Grote
+ * @version
+ */
 public class Testdata
 {
 	private LinkedList<Transaction> transactions;
 	private GregorianCalendar startDate, endDate;
 	private int sizeOfWarehouse;
 
+	/**
+	 * Erzeugt neue, zufällige Testdatensätze. Dabei wird geprüft, ob das Startdatum ein arbeitsfreier Tag ist und in diesem Fall am nächsten Arbeitstag mit der {@link de.hsw.warehouse.analysis.Testdata#generateTestData(Warehouse, GregorianCalendar, GregorianCalendar) Testdatengenerierung} begonnen.
+	 * @param warehouse Das zugrunde liegende {@link de.hsw.warehouse.model.Warehouse Lager}.
+	 * @param startDate Der Startzeitpunkt für die Erstellung von Testdatensätzen.
+	 * @param endDate Der Endzeitpunkt für die Erstellung von Testdatensätzen.
+	 */
 	public Testdata(Warehouse warehouse, GregorianCalendar startDate,
 			GregorianCalendar endDate)
 	{
@@ -44,6 +56,11 @@ public class Testdata
 		generateTestData(warehouse, startDate, endDate);
 	}
 
+	/**
+	 * {@link de.hsw.warehouse.analysis.Testdata#readFromDisk(Path) Liest} bereits vorhandene Testdaten aus dem angegebenen Pfad ein.
+	 * @param path Der Pfad zu der Datei, in welcher die Testdaten gespeichert sind.
+	 * @throws Exception Wenn die Datei nicht vorhanden ist oder keine Testdatensätze enthält.
+	 */
 	public Testdata(Path path) throws Exception
 	{
 		this.transactions = this.readFromDisk(path);
@@ -51,16 +68,30 @@ public class Testdata
 		this.endDate = this.transactions.getLast().getDate();
 	}
 	
+	/**
+	 * Gibt das Gesamtvolumen des zugrunde liegenden {@link de.hsw.warehouse.model.Warehouse Lagers} zurück.
+	 * @return Das Gesamtvolumen des zugrunde liegenden {@link de.hsw.warehouse.model.Warehouse Lagers}.
+	 */
 	public int getSizeOfWarehouse()
 	{
 		return this.sizeOfWarehouse;
 	}
 
+	/**
+	 * Gibt eine Liste aller {@link de.hsw.warehouse.analysis.Transaction Lagerbewegungen}/Testdatensätze zurück.
+	 * @return Eine Liste aller Testdatensätze.
+	 */
 	public LinkedList<Transaction> getTransactions()
 	{
 		return transactions;
 	}
 	
+	/**
+	 * Gibt eine Liste aller {@link de.hsw.warehouse.analysis.Transaction Lagerbewegungen}/Testdatensätze zurück.
+	 * @param startOfPeriod Startpunkt des Zeitraums.
+	 * @param endOfPeriod Endpunkt des Zeitraums.
+	 * @return Eine Liste aller Testdatensätze in dem angegebenen Zeitraum.
+	 */
 	public List<Transaction> getTransactionsInPeriod(GregorianCalendar startOfPeriod, GregorianCalendar endOfPeriod)
 	{
 		int fromIndex = 0;
@@ -79,16 +110,33 @@ public class Testdata
 		return this.transactions.subList(fromIndex, toIndex);
 	}
 
+	/**
+	 * Gibt das Startdatum der Testdaten zurück.
+	 * @return Das Startdatum der Testdaten.
+	 */
 	public GregorianCalendar getStartDate()
 	{
 		return startDate;
 	}
 
+	/**
+	 * Gibt das Enddatum der Testdaten zurück.
+	 * @return Das Enddatum der Testdaten.
+	 */
 	public GregorianCalendar getEndDate()
 	{
 		return endDate;
 	}
 
+	/**
+	 * Diese Methode generiert die einzelnen Testdatensätze. Dabei wurden folgende Annahmen getroffen:<br>
+	 * 		&emsp;1. Ein Arbeitstag startet um 7:00 Uhr und dauert 8:00 Stunden.<br>
+	 * 		&emsp;2. An einem Tag finden maximal 20 {@link de.hsw.warehouse.analysis.Transaction Lagerbewegungen} statt.<br>
+	 * 		&emsp;3. Eine {@link de.hsw.warehouse.analysis.Transaction Lagerbewegung} besteht aus der {@link de.hsw.warehouse.model.Warehouse#store(int, int, GregorianCalendar) Ein}- bzw. {@link de.hsw.warehouse.model.Warehouse#store(int, int, GregorianCalendar) Aus}lagerung von max. 20 {@link de.hsw.warehouse.model.Article Artikeln} eines Typen.
+	 * @param warehouse Das zugrunde liegende {@link de.hsw.warehouse.model.Warehouse Lager}.
+	 * @param startDate Der Startzeitpunkt für die Erstellung von Testdatensätzen.
+	 * @param endDate Der Endzeitpunkt für die Erstellung von Testdatensätzen.
+	 */
 	private void generateTestData(Warehouse warehouse,
 			GregorianCalendar startDate, GregorianCalendar endDate)
 	{
@@ -131,6 +179,10 @@ public class Testdata
 		}
 	}
 	
+	/**
+	 * Hier werden die Testdaten an den angegebenen Pfad auf die Festplatte geschrieben.
+	 * @param path Der Pfad zu der zu schreibenden Datei.
+	 */
 	public void writeToDisk(Path path)
 	{
 		String[] lines = new String[this.getTransactions().size() + 1];
@@ -148,6 +200,12 @@ public class Testdata
 		}
 	}
 	
+	/**
+	 * Hier werden Testdaten von der Festplatte eingelesen.
+	 * @param path Der Pfad zu der Datei, in welcher die Testdaten gespeichert sind.
+	 * @return Eine Liste aller Testdatensätze.
+	 * @throws Exception
+	 */
 	public LinkedList<Transaction> readFromDisk(Path path) throws Exception
 	{
 		String[] lines = null;
