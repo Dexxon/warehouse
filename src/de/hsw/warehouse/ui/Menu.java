88244f622d0dce5b56c7672a3f9452fbb8bc750f
Constantin <Constantin@Constantin-PC>
2014-05-29 09:11:33 +0200

    Initial documentation of Menu.java

--- src/de/hsw/warehouse/ui/Menu.java
+++ src/de/hsw/warehouse/ui/Menu.java
@@ -6,11 +6,21 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * Diese Klasse repräsentiert ein Menü. Dieses kann mehrere {@link de.hsw.warehouse.ui.MenuItem Menüeintrage} beinhalten.<br>
+ * Da ein Menü als einzelne Zeile in einem anderen Menü angezeigt wird, ist es gleichzeitig ein Menüeintrag und kann eine Unterklasse der Klasse {@link de.hsw.warehouse.ui.MenuItem} sein.
+ * @author Timo Rodenwaldt
+ * @version 
+ * @see <a href="http://bytes.com/topic/java/insights/870013-text-based-menus">http://bytes.com/topic/java/insights/870013-text-based-menus</a>
+ */
 public class Menu extends MenuItem
 {
 	ArrayList<MenuItem> menuItems;
 	String title;
 
+	/**
+	 * Der "Beenden"-Eintrag. Wird dieser ausgewählt, beendet sich das Programm normal.
+	 */
 	static final MenuItem exit = new MenuItem("Beenden", new Runnable()
 	{
 		public void run()
@@ -19,6 +29,10 @@ public class Menu extends MenuItem
 		}
 	});
 
+	/**
+	 * Der "Zurück"-Eintrag. Dieser hat keinen ausführbaren Code und wird mit dem Konstruktor {@link de.hsw.warehouse.ui.MenuItem#MenuItem(String) MenuItem(String)} erstellt.
+	 * Aus diesem Grund exisitiert dieser Konstruktor und ist nur in dieser Klasse und der Oberklasse sichtbar, damit er nicht woanders aufgerufen werden kann.
+	 */
 	static final MenuItem back = new MenuItem("Zurück");
 
 	Menu(String title, MenuItem... menuItems)
@@ -26,6 +40,13 @@ public class Menu extends MenuItem
 		this(title, false, true, menuItems);
 	}
 
+	/**
+	 * Erstellt ein Menü mit den angegebenen Einträgen und dem angegebenen Titel.
+	 * @param title Der Titel des Menüs.
+	 * @param back Gibt an, ob ein {@link de.hsw.warehouse.ui.Menu#back "Zurück"-Eintrag} vorhanden sein soll.
+	 * @param exit Gibt an, ob ein {@link de.hsw.warehouse.ui.Menu#exit "Beenden"-Eintrag} vorhanden sein soll.
+	 * @param menuItems Die hinzuzufügenden {@link de.hsw.warehouse.ui.MenuItem Menüeinträge}.
+	 */
 	Menu(String title, boolean back, boolean exit, MenuItem... menuItems)
 	{
 		super(title);
@@ -38,6 +59,9 @@ public class Menu extends MenuItem
 
 	}
 
+	/**
+	 * Gibt alle {@link de.hsw.warehouse.ui.MenuItem Menüeinträge} mit vorangestellter Nummerierung zeilenweise auf der Konsole aus.
+	 */
 	void display()
 	{
 		int option = 0;
@@ -48,6 +72,11 @@ public class Menu extends MenuItem
 		System.out.print("Wählen Sie eine Option: ");
 	}
 
+	/**
+	 * Fragt den Benutzer nach einer Eingabe, mit der er einen  {@link de.hsw.warehouse.ui.MenuItem Menüeintrag} auswählen kann.
+	 * @return Der ausgewählte  {@link de.hsw.warehouse.ui.MenuItem Menüeintrag}.
+	 * @throws IOException Wenn bei der Eingabe Fehler auftreten.
+	 */
 	private MenuItem prompt() throws IOException
 	{
 		int option;
@@ -67,6 +96,12 @@ public class Menu extends MenuItem
 		}
 	}
 
+	/**
+	 * Der ausführbare Code des Menüs.<br>
+	 * Der Benutzer wird zunächst nach der Eingabe gefragt. Wenn ein  {@link de.hsw.warehouse.ui.MenuItem Menüeintrag} ausgewählt wurde, wird dessen Code ausgeführt.
+	 * Handelt es sich bei dem  {@link de.hsw.warehouse.ui.MenuItem Menüeintrag} um ein Untermenü, wird diese Methode für jenes Menü ausgeführt. Handelt es sich um einen  {@link de.hsw.warehouse.ui.MenuItem Menüeintrag}, wird dessen Code ausgeführt.
+	 * Ist dieser Code nicht vorhanden (das bedeutet es handelt sich um den "Zurück"-Eintrag), wird die Methode verlassen und in das Obermenü zurückgekehrt.
+	 */
 	@Override
 	public void run()
 	{
@@ -80,6 +115,11 @@ public class Menu extends MenuItem
 		}
 	}
 
+	/**
+	 * Fügt dem Menü einen  {@link de.hsw.warehouse.ui.MenuItem Menüeintrag} hinzu.
+	 * @param menueItem Der hinzuzufügende  {@link de.hsw.warehouse.ui.MenuItem Menüeintrag}.
+	 * @return
+	 */
 	Menu add(MenuItem menueItem)
 	{
 		menuItems.add(menueItem);
