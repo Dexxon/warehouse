3c7d8064afc449a5f49827cf8b89f2e3be07ee48
Constantin <Constantin@Constantin-PC>
2014-05-29 10:51:16 +0200

    Minor changes

--- src/de/hsw/warehouse/analysis/Analysis.java
+++ src/de/hsw/warehouse/analysis/Analysis.java
@@ -108,6 +108,10 @@ public class Analysis {
 	 * @param data Die auszuwertenden Testdaten.
 	 */
 	public static void differenceOfPeriod(GregorianCalendar startDate, GregorianCalendar endDate, Testdata data) {
+		
+		if(data == null) {
+			throw new NullPointerException();
+		}
 	
 		int quantityStart[] = calculateQuantityPerDay(startDate, data);
 		int quantityEnd[] = calculateQuantityPerDay(endDate, data);
@@ -117,9 +121,6 @@ public class Analysis {
 	
 		for (int articleID = 0; articleID < Assortment.getSize(); articleID++) {
 			System.out.printf("%-13d %-60s %-15d\n", articleID, Assortment.getArticleName(articleID), (quantityEnd[articleID] - quantityStart[articleID]));
-			/*System.out.println(articleID + 
-					"\t" + Assortment.getName(articleID) + 
-					"\t" + (quantityEnd[articleID] - quantityStart[articleID]));*/
 		}
 	}
 
@@ -130,26 +131,34 @@ public class Analysis {
 	 * @param data Die auszuwertenden Testdaten.
 	 */
 	public static void turnFrequency(GregorianCalendar startDate, GregorianCalendar endDate, Testdata data){
-		Integer[] disposals = new Integer[Assortment.getSize()];
-		for (int i =0; i < disposals.length; i++){
-			disposals[i]=0;
-		}
-			
 		
+		if(data == null) {
+			throw new NullPointerException();
+		}
+		
+		int[] disposals = new int[Assortment.getSize()];
+			
 		for (Transaction transaction : data.getTransactionsInPeriod(startDate, endDate)) {
 			if (transaction.getQuantity() < 0) {
 				disposals[transaction.getArticleID()] += transaction.getQuantity();
 			}
 		}
+		
 		int[] averageQuantity = calculateAverageQuantity(startDate, endDate, data);
 		float[] turnFrequencyPerArticle = new float[Assortment.getSize()];
+		
+		System.out.println("Umschlagshäufigkeit vom " + Util.parseDate(startDate) + " bis zum " + Util.parseDate(endDate) + ":\n");
 		System.out.printf("%-13s %-60s %-15s\n", "Artikelnummer", "Artikelname", "Umschlagshäufigkeit");
+		
 		for (int i = 0; i< Assortment.getSize(); i++){
+			
 			if (disposals[i] < 0 && averageQuantity[i] > 0){
 			turnFrequencyPerArticle[i] = (float)disposals[i] / averageQuantity[i] * -1;
 			System.out.printf("%-13d %-60s %.2f\n", i, Assortment.getArticleName(i), turnFrequencyPerArticle[i]);
 			}
-			else System.out.printf("%-13d %-60s" + " 0\n", i, Assortment.getArticleName(i));
+			else {
+				System.out.printf("%-13d %-60s" + " 0\n", i, Assortment.getArticleName(i));
+			}
 		}
 		
 		
@@ -174,6 +183,7 @@ public class Analysis {
 		System.out.printf("%-13s %-13s %-60s %-15s\n","Datum" , "Artikelnummer", "Artikelname", "Bestand");
 		
 		int days = calculateDays(startDate, endDate);
+		
 		for(int i = 0; i < days; i ++) {
 			System.out.printf("%-13s %-13d %-60s %-15d\n",Util.parseDate(tempDate), articleID, Assortment.getArticleName(articleID), calculateQuantityPerDay(tempDate, data)[articleID]);
 			tempDate.add(GregorianCalendar.DAY_OF_YEAR, 1);
@@ -189,26 +199,31 @@ public class Analysis {
 	 */
 	public static void stockUtilizationInPeriod(GregorianCalendar startDate, GregorianCalendar endDate, Testdata data) {
 		
+		if(data == null) {
+			throw new NullPointerException();
+		}
+		
 		GregorianCalendar currentDate = (GregorianCalendar) startDate.clone();
 		int utilization = 0;
-		if(data.getTransactions().getFirst().getDate().before(currentDate)){
-			for (Transaction transaction : data.getTransactionsInPeriod(data.getTransactions().getFirst().getDate(), currentDate)) {
-				utilization += transaction.getVolume();
-			}
+		
+		for (Transaction transaction : data.getTransactionsInPeriod(data.getTransactions().getFirst().getDate(), currentDate)) {
+			utilization += transaction.getVolume();
 		}
+		
 		int[] volumePerDay = new int[calculateDays(currentDate, endDate)];
+		System.out.println("Auslastung vom " + Util.parseDate(startDate) + " bis zum " + Util.parseDate(endDate) + ":\n");
 		System.out.printf("%-29s %-10s\n", "Datum: ", " Auslastung: ");
 		for (int i = 0; i < volumePerDay.length; i++){
 			int[] articleQuantities = calculateQuantityPerDay(currentDate, data);
+			
 			for (int j = 0; j < articleQuantities.length; j++){
 				volumePerDay[i] += articleQuantities[j]*Assortment.getArticleVolume(j);
 			}
-			//System.out.println("Datum: " + Util.parseDate(startDate) + " beträgt " + (float)volumePerDay[i]/data.getSizeOfWarehouse());
+			
 			System.out.printf("%-30s %.2f %-10s\n", Util.parseDate(currentDate), (float)volumePerDay[i]/data.getSizeOfWarehouse()*100, "%");
 			utilization += volumePerDay[i];
 			currentDate.add(GregorianCalendar.DAY_OF_YEAR, 1);	
 		}
-		//System.out.printf("Die durchschnittliche Auslastung ist: " + (float)utilization/volumePerDay.length/data.getSizeOfWarehouse() + "%\n");
 		System.out.printf("\n%-19s %.2f %-10s\n", "Durchschnittliche Auslastung: ", (float)utilization/volumePerDay.length/data.getSizeOfWarehouse()*100, "%");
 		System.out.println();
 		
